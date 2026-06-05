55cf3181d80b59f672bce86553eebb20c23a324e
QHMY114514 <1550187725@qq.com>
2026-06-05 22:48:36 +0800

    添加新字体

--- js/utils/options.js
+++ js/utils/options.js
@@ -136,9 +136,9 @@ function adjustMSDisp() {
 	options.msDisplay = MS_SETTINGS[(MS_SETTINGS.indexOf(options.msDisplay) + 1) % MS_SETTINGS.length];
 }
 
-const FONT_DISPLAYS = ["默认", "Sligoil", "Angus", "Step mono", "Unica One", "Mathd", "FoxSize", "Misans 中文字体大请耐心等待", "等距更纱黑体 中文字体大请耐心等待"];
+const FONT_DISPLAYS = ["默认", "Sligoil", "Angus", "Step mono", "Unica One", "Mathd", "FoxSize", "Misans 中文字体大请耐心等待", "等距更纱黑体 中文字体大请耐心等待", "荆南波波黑 中文字体大请耐心等待"];
 
-const FONT_SETTINGS = ["", "Sligoil", "Angus", "Stepmono", "Unica", "Mathd", "fox", "Misans", "DJGSHT"];
+const FONT_SETTINGS = ["", "Sligoil", "Angus", "Stepmono", "Unica", "Mathd", "fox", "Misans", "DJGSHT", "JNBBH"];
 
 function adjustFont() {
 	options.font = FONT_SETTINGS[(FONT_SETTINGS.indexOf(options.font) + 1) % FONT_SETTINGS.length];
